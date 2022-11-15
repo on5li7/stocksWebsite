3db8e1c228b06778561ab3569338eb7522e9608c
muradhaider <m_haider3@u.pacific.edu>
2022-11-14 16:18:16 -0800

    First commit for HTML in php file also just a test with some starting code

---
+++ HEAT.php
@@ -0,0 +1,12 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <link rel = "stylesheet"
+          href = "index-style.css">
+
+    <title> HEAT STOCK SCANNER </title>
+<body>
+
