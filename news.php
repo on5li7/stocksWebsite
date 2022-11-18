c490b8b4c7d989c548003b94d288991833f53a62
vivek kumar maheshwari <v_kumarmaheshwari@u.pacific.edu>
2022-11-17 20:08:22 -0800

    Added the news tab basic functionality. Where the table is scrollable.

--- news.php
+++ news.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <link rel = "stylesheet" href = "heatStyle.css">
+    <link rel = "stylesheet" href = "heatStyle.css?v=<?php echo time(); ?>">
     <script type="text/javascript" src="heat.js"></script>
 
     <title> NEWS </title>
@@ -19,3 +19,30 @@
 </div>
 
 <button class="openbtn" onclick="openNav()">&#9776; Toggle Options</button>
+
+<?php
+$key = "NZV999OQVDUJ6N1W";
+$url = "https://www.alphavantage.co/query?function=NEWS_SENTIMENT&topics=financial_markets, technology, ipo, earnings, finance&sort=LATEST&apikey=".$key;
+$ch = curl_init();
+curl_setopt($ch,CURLOPT_URL,$url);
+curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+$result=curl_exec($ch);
+curl_close($ch);
+$result=json_decode($result, true);
+echo '<pre>';
+$entries = $result["items"];
+?>
+<div class="newstable">
+<table>
+    <tr>
+        <?php
+        for($i=1;$i<$entries;$i++) {
+            $title = $result["feed"][$i]["title"];
+            $news_url = $result["feed"][$i]["url"];
+            echo("<td><strong> <a href=$news_url> $title </a> </strong></td>");
+            echo("<tr></tr>");
+        }
+        ?>
+    </tr>
+</table>
+</div>
