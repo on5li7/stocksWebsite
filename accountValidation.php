1891a5b2abae2aac6cc0ed7c43598bfd0b7b3079
muradhaider <m_haider3@u.pacific.edu>
2022-12-06 13:45:44 -0800

    Changed unnecessary repeated variable to inline variable

--- accountValidation.php
+++ accountValidation.php
@@ -82,8 +82,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 function test_input($data) {
     $data = trim($data);
     $data = stripslashes($data);
-    $data = htmlspecialchars($data);
-    return $data;
+    return htmlspecialchars($data);
 }
 ?>
 </body>
