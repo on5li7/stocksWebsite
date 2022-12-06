f5accb16159d8d5e138b316a9c64313ca87cee07
muradhaider <m_haider3@u.pacific.edu>
2022-12-05 20:31:50 -0800

    Cleaned up all php pages and added the correct toggles for all of them. Wanted to add dark mode in settings but I deleted it in this commit to make it cleaner.

--- heat.js
+++ heat.js
@@ -7,9 +7,3 @@ function openNav() {
 function closeNav() {
     document.getElementById("mySidepanel").style.width = "0";
 }
-
-function myFunction() {
-    let element = document.body;
-    element.classList.toggle("dark-mode");
-    element.classList.toggle("default");
-}
