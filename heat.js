205ce662839b96ca802191fa508f724603844caf
muradhaider <m_haider3@u.pacific.edu>
2022-12-08 12:57:55 -0800

    Adjusted JS file and fixed some css stuff for settings

--- heat.js
+++ heat.js
@@ -1,4 +1,6 @@
 /* Set the width of the sidebar to 250px (show it) */
+
+
 function openNav() {
     document.getElementById("mySidepanel").style.width = "250px";
 }
@@ -6,4 +8,9 @@ function openNav() {
 /* Set the width of the sidebar to 0 (hide it) */
 function closeNav() {
     document.getElementById("mySidepanel").style.width = "0";
+}
+
+function myFunction() {
+    let element = document.body;
+    element.classList.toggle("dark-mode");
 }
