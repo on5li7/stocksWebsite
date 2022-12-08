9cd36325805d2d00252ac7af0eb4c214c69c17fa
muradhaider <m_haider3@u.pacific.edu>
2022-12-08 13:02:57 -0800

    Cleaned up settings button and removed the settings file, dont want it anymore!

--- createAccount.php
+++ createAccount.php
@@ -15,7 +15,6 @@
     <a href="index.php">Home</a>
     <a href="news.php">News</a>
     <a href="screener.php">Screener</a>
-    <a href="settings.php">Settings</a>
 </div>
 
 <button class="openbtn" onclick="openNav()">&#9776; Toggle Options</button>
