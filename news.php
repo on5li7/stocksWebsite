6ac9f1071bcd97fb0d762771cf3f2958be53d172
vivek kumar maheshwari <v_kumarmaheshwari@u.pacific.edu>
2022-12-08 09:29:28 -0800

    Some more cleaning and styling

--- news.php
+++ news.php
@@ -7,7 +7,7 @@
 
     <title> NEWS </title>
 <body>
-<h1> NEWS </h1>
+<h1 class="newsheading"> NEWS </h1>
 
 <div id="mySidepanel" class="sidePanel">
     <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
@@ -22,29 +22,40 @@
 
 
 <?php
+    function news_processing($url){
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $result = curl_exec($ch);
+        curl_close($ch);
+        $result = json_decode($result, true);
+        echo '<pre>';
+        $entries = $result["items"];
+        display_news($result,$entries);
+    }
+
 $key = "NZV999OQVDUJ6N1W";
-$url = "https://www.alphavantage.co/query?function=NEWS_SENTIMENT&topics=financial_markets, technology, ipo, earnings, finance&sort=LATEST&apikey=".$key;
-$ch = curl_init();
-curl_setopt($ch,CURLOPT_URL,$url);
-curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
-$result=curl_exec($ch);
-curl_close($ch);
-$result=json_decode($result, true);
-echo '<pre>';
-$entries = $result["items"];
+$url = "https://www.alphavantage.co/query?function=NEWS_SENTIMENT&topics=financial_markets, technology, ipo, earnings, finance&sort=LATEST&apikey=" . $key;
+news_processing($url);
+
 ?>
 
-<div class="newstable">
-<table>
-    <tr>
-        <?php
-        for($i=1;$i<$entries;$i++) {
-            $title = $result["feed"][$i]["title"];
-            $news_url = $result["feed"][$i]["url"];
-            echo("<td><strong> <a href=$news_url> $title </a> </strong></td>");
-            echo("<tr></tr>");
-        }
-        ?>
-    </tr>
-</table>
-</div>
+<?php function display_news($result, $entries){?>
+    <div class="newstable">
+        <table id="customers">
+            <tr>
+                <?php
+                    for($i=1;$i<$entries;$i++) {
+                        $title = $result["feed"][$i]["title"];
+                        $news_url = $result["feed"][$i]["url"];
+                        echo("<td><strong> <a href=$news_url> $title </a> </strong></td>");
+                        echo("<tr></tr>");
+                    }
+
+                ?>
+            </tr>
+        </table>
+    </div>
+<?php
+}
+?>
