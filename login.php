ecd5dcb29c29ba44966e3051539a94cca20e7d58
vivek kumar maheshwari <v_kumarmaheshwari@u.pacific.edu>
2022-12-08 07:52:54 -0800

    Added some styling to login page

--- login.php
+++ login.php
@@ -7,7 +7,6 @@
 
     <title> HEAT STOCK LOG IN</title>
 <body>
-<h1> LOG IN </h1>
 
 <div id="mySidepanel" class="sidePanel">
     <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
@@ -19,27 +18,24 @@
 </div>
 
 <button class="openbtn" onclick="openNav()">&#9776; Toggle Options</button>
-<div class = "loginFieldSet">
-<fieldset>
-    <h1>Log In to HEAT.NET</h1>
-    <label>
-        <input type="text" name="username" placeholder="Enter Username"> </label>
-    <br>
-    <br>
-       <label> <input type="text" name="password" placeholder="Enter Password">
-    </label>
-    <br>
-    <br>
-    <a href="">Forgot Password?</a>
-    <br>
-    <a href="createAccount.php">Create an Account!</a>
-    <br>
-    <br>
-    <button type="submit" onclick="location.href='index.php'">Log In</button>
-</fieldset>
-</div>
 
 
+<form action="/index.php" method="post">
+    <div class="loginform">
+        <h1 class="loginheading">Log In</h1>
+        <label>
+            <input type="text" name="username" placeholder="USERNAME"> </label>
+        <br>
+           <label> <input type="text" name="password" placeholder="PASSWORD">
+        </label>
+        <br>
+        <button type="submit" class="loginbutton"> LOG IN </button>
+        <br>
+        <br>
+        <a href="createAccount.php" class="acc">Not registered? Create an Account!</a>
+    </div>
+
+</form>
 
 </body>
 </html>
